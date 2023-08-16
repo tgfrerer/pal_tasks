7e35d1f5669571aaaa78d2df70c568626639310e
tgfrerer <tim@poniesandlight.co.uk>
2023-08-16 11:59:49 +0100

    update main.cpp to respond to arguments

--- main.cpp
+++ main.cpp
@@ -7,17 +7,25 @@ int raytracer_main( int argc, char** argv ); // from raytracer.cpp
 
 int main( int argc, char** argv ) {
 
-	raytracer_main( argc, argv );
+	// raytracer_main( argc, argv );
 
-	return 0;
+	// argument 0 is the path to the application
+	// argument 1 is the number of threads specified, if any
+	const int   num_threads = argc >= 2 ? atoi( argv[ 1 ] ) : -1;
+	char const* choices     = argc >= 3 ? argv[ 2 ] : "001";
+
+	// return 0;
 
 	// Create a scheduler with as many hardware threads as possible
 	//  0 ... No worker threads, just one main thread
 	//  n ... n number of worker threads
 	// -1 ... As many worker threads as cpus, -1
-	Scheduler* scheduler = Scheduler::create( -1 );
+	Scheduler* scheduler = Scheduler::create( num_threads );
 
-	if ( 1 ) {
+	if ( choices && *choices++ == '1' ) {
+
+		std::cout << std::endl;
+		std::cout << "- - - - -SCENARIO 1" << std::endl;
 
 		TaskList tasks{};
 		auto     task_generator = []( Scheduler* scheduler, int i ) -> Task {
@@ -62,8 +70,10 @@ int main( int argc, char** argv ) {
 	}
 
 	// --- vanilla scenario
-	if ( true ) {
+	if ( choices && *choices++ == '1' ) {
 
+		std::cout << std::endl;
+		std::cout << "- - - - -SCENARIO 2" << std::endl;
 		TaskList tasks{};
 		auto     task_generator = []( int i ) -> Task {
             std::cout << "doing some work: " << i++ << std::endl;
@@ -95,7 +105,11 @@ int main( int argc, char** argv ) {
 
 	std::cout << "MAIN thread is: " << std::hex << std::this_thread::get_id() << std::endl;
 
-	if ( true ) {
+	if ( choices && *choices++ == '1' ) {
+
+		std::cout << std::endl;
+		std::cout << "- - - - - SCENARIO 3" << std::endl;
+
 		/*
 		 * This test is for whether we can issue tasks from within our
 		 * current task system.
@@ -108,23 +122,23 @@ int main( int argc, char** argv ) {
 
             std::this_thread::sleep_for( std::chrono::microseconds( rand_r( &i ) % 55000 ) );
 
-            auto inner_coro_generator = []( uint i, int j ) -> Task {
-                //    std::cout << "\t executing inner coroutine: " << std::dec << i << ":" << j++ << " on thread: " << std::hex << std::this_thread::get_id() << std::endl
-                //              << std::flush;
+			auto inner_coro_generator = []( uint i, int j ) -> Task {
+				//    std::cout << "\t executing inner coroutine: " << std::dec << i << ":" << j++ << " on thread: " << std::hex << std::this_thread::get_id() << std::endl
+				//              << std::flush;
 
-                std::this_thread::sleep_for( std::chrono::microseconds( rand_r( &i ) % 40000 ) );
-                // this yields control back to the await_suspend method, and to our scheduler
-                co_await suspend_task();
+				std::this_thread::sleep_for( std::chrono::microseconds( rand_r( &i ) % 40000 ) );
+				// this yields control back to the await_suspend method, and to our scheduler
+				co_await suspend_task();
 
-                std::this_thread::sleep_for( std::chrono::microseconds( rand_r( &i ) % 33000 ) );
-                //    std::cout << "\t executing inner coroutine: " << std::dec << i << ":" << j++ << " on thread: " << std::hex << std::this_thread::get_id() << std::endl;
-                co_return;
-            };
+				std::this_thread::sleep_for( std::chrono::microseconds( rand_r( &i ) % 33000 ) );
+				//    std::cout << "\t executing inner coroutine: " << std::dec << i << ":" << j++ << " on thread: " << std::hex << std::this_thread::get_id() << std::endl;
+				co_return;
+			};
 
 			uint32_t num_tasks = rand_r( &i ) % 10;
 
 			// Create a task list for tasks which are spun off from within this task
-            TaskList inner_task_list{};
+			TaskList inner_task_list{};
 
 			for ( int j = 0; j != num_tasks; j++ ) {
 				inner_task_list.add_task( inner_coro_generator( i, j * 10 ) );
@@ -164,7 +178,7 @@ int main( int argc, char** argv ) {
 		scheduler->wait_for_task_list( another_task_list );
 	}
 
-	std::cout << "Back with main program." << std::endl
+	std::cout << "- - - - - Back with main program." << std::endl
 	          << std::flush;
 	std::cout << "MAIN thread is: " << std::hex << std::this_thread::get_id() << std::endl;
 
