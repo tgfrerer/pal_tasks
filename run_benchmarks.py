562bb093e3b411b703fc3424274563e8f419d5a8
tgfrerer <tim@poniesandlight.co.uk>
2023-09-01 16:07:41 +0100

    uncomment missing lines in benchmarks script
    variant 1
    
    variant 2
    
    run 3 iterations

--- run_benchmarks.py
+++ run_benchmarks.py
@@ -62,16 +62,16 @@ if status != 0:
 p=subprocess.run('git show --format="%h" --no-patch'.split(" "), capture_output=True)
 git_hash = p.stdout.split(b'"')[1].decode('utf-8')
 
-# for i in range(1,33):
-#     print("Starting run {0}".format(i))
-#     p = subprocess.run(("sudo /usr/bin/perf stat -o ./perf/perf_{0}.csv -x \\t -r 1 ./tasks {0} {1} > ./perf/out_{0}.txt".format(i, invocation_param)) .split(" "), capture_output=True, cwd=working_directory)
-#     f = open("{0}/perf/out_{1}.txt".format(working_directory,str(i)), 'wb')
-#     f.write(p.stdout)
-#     f.close()
-#     if p.returncode != 0 :
-#         print("program errored out: {}".format(p.returncode))
-#         exit(p.returncode)
-#     print("Completed run {0}\n".format(i))
+for i in range(1,33):
+     print("Starting run {0}".format(i))
+     p = subprocess.run(("sudo /usr/bin/perf stat -o ./perf/perf_{0}.csv -x \\t -r 3 ./tasks {0} {1} > ./perf/out_{0}.txt".format(i, invocation_param)) .split(" "), capture_output=True, cwd=working_directory)
+     f = open("{0}/perf/out_{1}.txt".format(working_directory,str(i)), 'wb')
+     f.write(p.stdout)
+     f.close()
+     if p.returncode != 0 :
+         print("program errored out: {}".format(p.returncode))
+         exit(p.returncode)
+     print("Completed run {0}\n".format(i))
 
 # for all .csv files in directory, fetch the filename and the first cell in the third line and add this as a line in the summary file
 
